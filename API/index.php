e051469b726cec2874f12ce8c5b8e019da3fe162
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-20 16:39:36 +0200

    add main structure for logic

---
+++ API/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$database_path = __DIR__ . '/../database/task.json';
+
+$json_data = file_get_contents($database_path);
+
+$tasks = json_decode($json_data, true);
+
+header('Content-Type: application/json');
+
+echo json_encode($tasks);
--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: "vue ok",
+      newTask: "",
+      tasks: [],
     };
+  },
+
+  created() {
+    axios.get("http://localhost/PHP-TODO-LIST-JSON/API/").then((res) => {
+      this.tasks = res.data;
+    });
   },
 }).mount("#root");
